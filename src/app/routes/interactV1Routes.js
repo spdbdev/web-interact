a57e5a81c129baa6a3b817040e4c8f790d1a070e
aplennie <alinapavel@rogers.com>
2022-10-12 00:19:55 -0400

    routing default jumbo sidebar

--- src/app/routes/interactV1Routes.js
+++ src/app/routes/interactV1Routes.js
@@ -1,46 +1,41 @@
 import React from "react";
-import CryptoDashboard from "../pages/dashboards/crypto/CryptoDashboard";
 import ListingDashboard from "../pages/dashboards/listing/ListingDashboard";
-import CrmDashboard from "../pages/dashboards/crm/CrmDashboard";
 import IntranetDashboard from "../pages/dashboards/intranet/IntranetDashboard";
-import EcommerceDashboard from "../pages/dashboards/ecommerce/EcommerceDashboard";
-import NewsDashboard from "../pages/dashboards/news/NewsDashboard";
-import MiscDashboard from "../pages/dashboards/misc/MiscDashboard";
 
 // Interactv1
-import SignInPage from '@interact/Pages/SignUpPage/SignInPage';
+import SignInPage from "Pages/SignUpPage/SignInPage";
 import UserProfile from "app/pages/users/user-profile";
-import UserProfilePage from "@interact/Pages/UserProfilePage/UserProfilePage";
-
-
+import UserProfilePage from "Pages/UserProfilePage/UserProfilePage";
+import CreateCampaignPage from "Pages/CreateCampaignPage/CreateCampaignPage";
+import CampaignPage from "Pages/CampaignPage/CampaignPage";
 
 const interactV1Routes = [
-    {
-        path: "/interact/signin",
-        element: <SignInPage/>,
-    },
-    {
-        path: "/interact/signup",
-        element: <ListingDashboard/>,
-    },
-    {
-        path: "/interact/user",
-        //element: <CrmDashboard/>,
-        element: <UserProfile/>, // use <UserProfilePage /> from @interact/Pages/UserProfilePages to see the old one.
-    },
-    {
-        path: "/interact/userold",
-        //element: <CrmDashboard/>,
-        element: <UserProfilePage/>,  
-    },
-    {
-        path: "/interact/campaign",
-        element: <IntranetDashboard/>,
-    },
-    {
-        path: "/interact/createCampaign",
-        element: <EcommerceDashboard/>,
-    },
+  {
+    path: "/interact/signin",
+    element: <SignInPage />,
+  },
+  {
+    path: "/interact/signup",
+    element: <ListingDashboard />,
+  },
+  {
+    path: "/interact/user",
+    //element: <CrmDashboard/>,
+    element: <UserProfile />, // use <UserProfilePage /> from @interact/Pages/UserProfilePages to see the old one.
+  },
+  {
+    path: "/interact/userold",
+    //element: <CrmDashboard/>,
+    element: <UserProfilePage />,
+  },
+  {
+    path: "/interact/campaign",
+    element: <CampaignPage />,
+  },
+  {
+    path: "/interact/createCampaign",
+    element: <CreateCampaignPage />,
+  },
 ];
 
 export default interactV1Routes;
