cb081c93f870b87e1aa9b3c15fda79a72b0ef96b
VirgaOptima <101373277+VirgaOptima@users.noreply.github.com>
2022-11-18 16:43:07 -0500

    tiny text edit

--- src/@interact/Pages/UserProfilePage/FollowedCampaigns.js
+++ src/@interact/Pages/UserProfilePage/FollowedCampaigns.js
@@ -119,7 +119,7 @@ function FollowedCampaigns() {
 		<br></br>
 		<CampaignsRow
 			currentCampaigns={currentCampaigns}
-			heading="Live campaigns"
+			heading="Followed campaigns"
 		/>
 		<CampaignsRow
 			currentCampaigns={interactionCampaigns}
