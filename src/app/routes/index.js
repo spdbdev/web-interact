4b7be3c14689c19d1e041688579ead2fdb06f718
VirgaOptima <101373277+VirgaOptima@users.noreply.github.com>
2022-11-18 10:59:52 -0500

    fixed routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -4,18 +4,6 @@ import { Navigate } from "react-router-dom";
 import oAuthRoutes from "./oAuthRoutes";
 
 const routes = [
-  {
-    path: "/",
-    element: <Navigate to={"/a/signin"} />,
-  },
-  {
-    path: "/signin",
-    element: <Navigate to={"/a/signin"} />,
-  },
-  {
-    path: "/signup",
-    element: <Navigate to={"/a/signup"} />,
-  },
   ...oAuthRoutes,
   ...interactV1Routes,
 ];
