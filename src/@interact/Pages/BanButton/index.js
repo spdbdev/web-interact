d835ca6b29fc778a566fca8fe5e452d701ff7107
VirgaOptima <101373277+VirgaOptima@users.noreply.github.com>
2022-11-25 17:58:33 -0500

    updated ban button

--- src/@interact/Pages/BanButton/index.js
+++ src/@interact/Pages/BanButton/index.js
@@ -1,5 +1,5 @@
-import InteractButton from "@interact/Components/Button/InteractButton";
 import Span from "@jumbo/shared/Span";
+import { LoadingButton } from "@mui/lab";
 import { banUserFromCampaign } from "../../../firebase";
 export function BanButton({ user, targetUser,sx }) {
 
@@ -14,11 +14,33 @@ export function BanButton({ user, targetUser,sx }) {
 	}
 
 	return (
-		<InteractButton sx={sx} variant={selectedState ? "contained" : "outlined"}
-			disabled={user?.id === targetUser?.id ? true : false} onClick={() => onBanButtonClicked()}>
-            <Span sx={{ color: selectedState ? "primary.contrastText" : "primary.main" }} >
+        <LoadingButton
+            sx={{
+                ":hover": {
+                background:
+                    "linear-gradient(-90deg, #FF0000 -37.69%, #DD00FF 109.93%)", // theme.palette.primary.main
+                color: "white",
+                },
+                background: "linear-gradient(90deg, #8747f0 -8.69%, #e433ff 109.93%)",
+                borderColor: "primary.main",
+                fontWeight: 500,
+                textTransform: "none",
+                borderRadius: "11px 3px",
+                borderWidth: 1,
+                px: 0,
+                marginLeft: 0.69,
+                py: 0.769,
+            }}
+            svariant={selectedState ? "contained" : "outlined"}
+			
+            //disableElevation
+            variant={"contained"}
+            type="submit"
+    
+        disabled={user?.id === targetUser?.id ? true : false} onClick={() => onBanButtonClicked()}>
+            <Span sx={{ color: selectedState ? "#FFFFFF" : "#FFFFFF" }} >
                 {selectedState ? "Banned" : "Ban"}
             </Span>
-		</InteractButton>
+    </LoadingButton>
 	);
 }
